74e8c59538f44db3c5c24630698d89d82f0125ee
Karolina Sowinska <32458081+karolina-sowinska@users.noreply.github.com>
2020-04-05 00:48:16 +0100

    cleaning up

--- credentials.py
+++
@@ -1,6 +0,0 @@
-USER = "k.m.sowinska@gmail.com"
-PWD = "OrangutanyTrzy333*"
-
-# Every time set the  GOOGLE_APPLICATION_CREDENTIALS VARIABLE in the command line:
- 
-# export GOOGLE_APPLICATION_CREDENTIALS="/mnt/c/Users/Karolina/Documents/Code/Hackyeah/Good-Deeds-Voicemail-4e9d6280f7b7.json"
